f7552faff3b76548696fe90c1fe8fe0af3bc07e6
HunterX012 <114446800+HunterX012@users.noreply.github.com>
2024-03-06 00:30:07 -0400

    Update ggsg.lua

--- ggsg.lua
+++ ggsg.lua
@@ -1,46 +1,49 @@
-local Services 						= setmetatable({}, { __index = function(Self, Key) return game.GetService(game, Key) end })
-local Client 						= Services.Players.LocalPlayer
-local SMethod 						= (WebSocket and WebSocket.connect)
+local Services = setmetatable({}, { __index = function(Self, Key) return game:GetService(Key) end })
+local Client = Services.Players.LocalPlayer
+local SMethod = (WebSocket and WebSocket.connect)
 
-if not SMethod then return Client:Kick("Executor is too shitty.") end
+if not SMethod then 
+    return Client:Kick("Executor is too shitty.") 
+end
 
-local Main 							= function()
-	local Success, WebSocket 		= pcall(SMethod, "ws://" .. _G.ip .. ":9000/")
-    local Closed                    = false
+local Main = function()
+    local Success, WebSocket = pcall(SMethod, "ws://" .. _G.ip .. ":9000/")
+    local Closed = false
 
-	if not Success then return end
+    if not Success then return end
 
-	WebSocket:Send(Services.HttpService:JSONEncode({
-		Method						= "Authorization",
-		Name						= Client.Name
-	}))
+    WebSocket:Send(Services.HttpService:JSONEncode({
+        Method = "Authorization",
+        Name = Client.Name
+    }))
 
-	WebSocket.OnMessage:Connect(function(Unparsed)
-		local Parsed 				= Services.HttpService:JSONDecode(Unparsed)
-		
-		if (Parsed.Method == "Execute") then
-			local Function, Error 	= loadstring(Parsed.Data)
+    WebSocket.OnMessage:Connect(function(Unparsed)
+        local Parsed = Services.HttpService:JSONDecode(Unparsed)
 
-			if Error then return WebSocket:Send(Services.HttpService:JSONEncode({
-				Method				= "Error",
-				Message				= Error
-			}))	end
-			
-			Function()
-		end
-	end)
+        if (Parsed.Method == "Execute") then
+            local Function, Error = loadstring(Parsed.Data)
 
-    -- i fucking hate you electron
-	-- WebSocket.OnClose:Wait()
+            if Error then 
+                return WebSocket:Send(Services.HttpService:JSONEncode({
+                    Method = "Error",
+                    Message = Error
+                }))
+            end
 
-	WebSocket.OnClose:Connect(function()
-        Closed                       = true
+            Function()
+        end
+    end)
+
+    WebSocket.OnClose:Connect(function()
+        Closed = true
     end)
 
     repeat task.wait() until Closed
 end
 
 while task.wait(1) do
-	local Success, Error 			= pcall(Main)
-	if not Success then print(Error) end
+    local Success, Error = pcall(Main)
+    if not Success then 
+        print(Error) 
+    end
 end
